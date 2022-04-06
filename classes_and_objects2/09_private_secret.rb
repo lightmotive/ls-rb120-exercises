a00a9970343c5a4c8b7e5fedaf6ac7c92cbe506d
Andre Light <andrelight.al@gmail.com>
2022-04-06 23:28:49 +0000

    Explore 02.09: *Private Secret*

---
+++ classes_and_objects2/09_private_secret.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Person
+  attr_writer :secret
+
+  def share_secret
+    secret
+  end
+
+  private
+
+  attr_reader :secret
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+p person1.share_secret == 'Shh.. this is a secret!'
