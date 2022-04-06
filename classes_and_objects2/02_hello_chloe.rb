adc1a27f161d52877e90a858ecc897416ce491e9
Andre Light <andrelight.al@gmail.com>
2022-04-06 22:48:18 +0000

    Complete 02.02: *Hello, Chloe!*

---
+++ classes_and_objects2/02_hello_chloe.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def rename(name)
+    self.name = name
+  end
+end
+
+kitty = Cat.new('Sophie')
+p kitty.name == 'Sophie'
+kitty.rename('Chloe')
+p kitty.name == 'Chloe'
