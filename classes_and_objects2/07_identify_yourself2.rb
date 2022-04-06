da0058b3eb9ea81b588e5a0428ce9b225ee8aa5a
Andre Light <andrelight.al@gmail.com>
2022-04-06 23:24:38 +0000

    Explore 02.07: *Identify Yourself (Part 2)*

---
+++ classes_and_objects2/07_identify_yourself2.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Cat
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    "I'm #{name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+puts kitty # => "I'm Sophie!"
