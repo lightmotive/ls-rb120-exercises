63500d20094014b752bf6682bf0e0308312a84f2
Andre Light <andrelight.al@gmail.com>
2022-04-13 23:12:18 +0000

    Complete 03.09: *Method Lookup, Part 3*

---
+++ 03_inheritance/09_method_lookup3.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Flyable
+  def fly
+    "I'm flying!"
+  end
+end
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+  include Flyable
+end
+
+bird1 = Bird.new('Red')
+bird1.color
+
+# Method Lookup Path through `color` invocation:
+# Bird
+# Flyable
+# Animal
