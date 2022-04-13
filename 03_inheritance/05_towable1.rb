504e9999e43cd198be1723318fa9ff6999e1953f
Andre Light <andrelight.al@gmail.com>
2022-04-13 22:49:05 +0000

    Complete 03.06: *Towable, Part 2*

--- 03_inheritance/05_towable1.rb
+++ 03_inheritance/05_towable1.rb
@@ -2,16 +2,28 @@
 
 module Towable
   def tow
-    puts 'I can tow a trailer!'
+    'I can tow a trailer!'
   end
 end
 
-class Truck
+class Vehicle
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
+  end
+end
+
+class Truck < Vehicle
   include Towable
 end
 
-class Car
+class Car < Vehicle
 end
 
-truck1 = Truck.new
-truck1.tow
+truck1 = Truck.new(1994)
+puts truck1.year
+puts truck1.tow
+
+car1 = Car.new(2006)
+puts car1.year
