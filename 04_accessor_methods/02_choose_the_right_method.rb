d3d81356d64e5dad7b14a9c4319ebc994b248738
Andre Light <andrelight.al@gmail.com>
2022-04-14 23:08:46 +0000

    Complete 04.02: *Choose the Right Method*

---
+++ 04_accessor_methods/02_choose_the_right_method.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Person
+  attr_accessor :name, :phone_number
+  private :phone_number
+end
+
+person1 = Person.new
+person1.name = 'Jessica'
+person1.phone_number = '0123456789'
+puts person1.name
