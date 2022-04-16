cde763317cbee8a595f94663dc098dd31627b80c
Andre Light <andrelight.al@gmail.com>
2022-04-16 22:55:10 +0000

    Explore 05.02: *What's the Output?*

--- 05_easy1/02_whats_the_output.rb
+++ 05_easy1/02_whats_the_output.rb
@@ -16,3 +16,13 @@ puts fluffy.name
 puts fluffy
 puts fluffy.name
 puts name
+
+name = 42
+fluffy = Pet.new(name)
+name += 1
+# Line 22 doesn't affect the class state because it's simply reassigning the
+# local variable `name` to a new Integer object in memory (`name = name + 1`).
+puts fluffy.name
+puts fluffy
+puts fluffy.name
+puts name
