5ddd99c0b573e63804ed58509ae8154ad882404e
Andre Light <andrelight.al@gmail.com>
2022-04-16 18:33:58 +0000

    Complete 04.09: *Calculated Age*

---
+++ 04_accessor_methods/09_calculated_age.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Person
+  def age=(age)
+    @age = age * 2
+  end
+
+  def age
+    @age * 2
+  end
+end
+
+person1 = Person.new
+person1.age = 20
+puts person1.age
