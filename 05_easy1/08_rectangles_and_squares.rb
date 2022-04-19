4eed803b4f21d3ef6b7a4f5b71f4f175af21f925
Andre Light <andrelight.al@gmail.com>
2022-04-19 22:54:47 +0000

    Complete 05.08: *Rectangles and Squares*

---
+++ 05_easy1/08_rectangles_and_squares.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Rectangle
+  def initialize(height, width)
+    @height = height
+    @width = width
+  end
+
+  def area
+    @height * @width
+  end
+end
+
+class Square < Rectangle
+  def initialize(side_length)
+    super(side_length, side_length)
+  end
+end
+
+square = Square.new(5)
+puts "area of square = #{square.area}"
