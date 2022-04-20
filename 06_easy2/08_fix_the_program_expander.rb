76a2188fffb95829b6980da6d371d058879890dc
Andre Light <andrelight.al@gmail.com>
2022-04-20 22:17:48 +0000

    # Complete 06.08: *Fix the Program - Expander*
    - It is legal to use `self` with private methods as of Ruby v2.7.
      - However, `self` isn't needed here, so let's exclude it.

---
+++ 06_easy2/08_fix_the_program_expander.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Expander
+  def initialize(string)
+    @string = string
+  end
+
+  def to_s
+    expand(3)
+  end
+
+  private
+
+  def expand(n)
+    @string * n
+  end
+end
+
+expander = Expander.new('xyz')
+puts expander
