daf5a74d10a49c051d852e2548112fcfe5612862
Andre Light <andrelight.al@gmail.com>
2022-06-19 19:08:30 +0000

    Explain Debugging Exercise 3

---
+++ 08_debugging/03_wish_you_were_here.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+end
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location
+# expected: true
+# actual: false
+# Reason: `BasicObject#==`, the default implementation in this case, returns
+# `true` only if the objects point to the same object in memory.
+# Solution: implement `==` in `GeoLocation` to override `BasicObject`'s default.
