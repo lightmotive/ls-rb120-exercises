1d86f4caa4ef136beb14f3df29f4481ec744fb38
Andre Light <andrelight.al@gmail.com>
2022-06-19 18:55:09 +0000

    Complete Debugging Exercise 2

---
+++ 08_debugging/02_animal_kingdom.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    # super
+    # Line 4 raises an exception due to line 39:
+    # `super` without parentheses or arguments forwards all 3 calling method
+    # parameters as args to `Bird#initialize`, which expects only 2 args.
+    # Solution: specify the args to forward:
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
