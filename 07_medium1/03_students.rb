143152ad91e61c22491243cc3ca905fed7bad903
Andre Light <andrelight.al@gmail.com>
2022-06-21 22:52:52 +0000

    Initialize Medium 1 Exercise 3

---
+++ 07_medium1/03_students.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Student
+  def initialize(name, year)
+    @name = name
+    @year = year
+  end
+end
+
+class Graduate
+  def initialize(name, year, parking)
+  end
+end
+
+class Undergraduate
+  def initialize(name, year)
+  end
+end
