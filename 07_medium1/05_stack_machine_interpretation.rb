9e212db9b52211d46f87d99aadeb363a417ae1e1
Andre Light <andrelight.al@gmail.com>
2022-06-22 23:23:12 +0000

    Complete Medium 1 Exercise 5
    - Some modifications and additional features.

--- 07_medium1/05_stack_machine_interpretation.rb
+++ 07_medium1/05_stack_machine_interpretation.rb
@@ -1,124 +1,161 @@
 # frozen_string_literal: true
 
-class Minilang
-end
-
-# Migrate this procedural version (RB101 Exercises, Med1.6) to OOP above:
-
-# * Understand the problem *
-# Input: string
-# Output: interpreted program may or may not print values to the screen
-#
-# A register contains a value, a stack contains commands, and the program
-# executes those commands to modify the register, perform math operations,
-# and print values.
-#
-# Basic logic:
-# - Split the command string
-# - Interpret commands
-#   - If command is not found in hash, it must be an integer.
-# - Perform operations sequentially
-
 require 'pp'
+require 'pry'
 
-def raise_custom_error(error_class, message, state)
-  message.concat("\n--State--\n#{state.pretty_inspect}")
-  raise error_class, message
-end
+# Stack Machine interpreter.
+class Minilang
+  attr_reader :stdout_log
 
-def validate_pop(state, detail: String.new)
-  if state[:stack].size.zero?
-    detail = String.new(" #{detail}") unless detail.empty?
-    raise_custom_error(
-      StandardError, "The stack was empty.#{detail}", state
-    )
+  def initialize(print_to_stdout: true)
+    @print_to_stdout = print_to_stdout
+    reset
   end
 
-  nil
-end
+  def eval(statement)
+    reset
+    self.statement = statement
+    self.expressions = statement.split
 
-def stack_operate!(state, operation)
-  validate_pop(state, detail: "Operation: #{operation}")
-  state[:register] = state[:register].send(operation, state[:stack].pop)
-end
+    execute(expressions.shift) until expressions.empty?
+  end
 
-@print_to_stdout = true
+  def exec_print
+    output = register.to_s
+    stdout_log.push(output)
+    puts output if print_to_stdout?
+  end
 
-COMMANDS = {
-  PRINT: lambda do |state|
-           output = state[:register].to_s
-           state[:stdout_log].push(output)
-           puts output if @print_to_stdout
-         end,
-  PUSH: ->(state) { state[:stack].push(state[:register]) },
-  POP: lambda do |state|
-    validate_pop(state)
-    state[:register] = state[:stack].pop
-  end,
-  ADD: ->(state) { stack_operate!(state, :+) },
-  SUB: ->(state) { stack_operate!(state, :-) },
-  MULT: ->(state) { stack_operate!(state, :*) },
-  DIV: ->(state) { stack_operate!(state, :/) },
-  MOD: ->(state) { stack_operate!(state, :%) },
-  default: lambda do |state, command_string|
-    state[:register] = Integer(command_string)
+  def exec_push
+    stack.push(register)
+  end
+
+  def exec_pop
+    validate_pop
+    self.register = stack.pop
+  end
+
+  def exec_add
+    operate(:+)
+  end
+
+  def exec_sub
+    operate(:-)
+  end
+
+  def exec_mult
+    operate(:*)
+  end
+
+  def exec_div
+    operate(:/)
+  end
+
+  def exec_mod
+    operate(:%)
+  end
+
+  def exec_integer(expression)
+    self.register = Integer(expression)
   rescue ArgumentError
-    raise_custom_error(
-      ArgumentError, "#{command_string} is not a valid command.", state
-    )
-  end
-  # Interpret integers (n) if command is not in the list above
-}.freeze
-
-def minilang_execute!(state, command_string, command)
-  return COMMANDS[:default].call(state, command_string) if command.nil?
-
-  command.call(state)
-
-  nil
-end
-
-def minilang_iterate!(state)
-  until state[:command_strings].empty?
-    command_string = state[:command_strings].shift
-    command = COMMANDS.fetch(command_string.to_sym, nil)
-    minilang_execute!(state, command_string, command)
+    raise_error_with_state(ArgumentError, "#{expression} is not a valid expression.")
   end
 
-  nil
+  private
+
+  attr_accessor :register, :expressions, :statement
+  attr_reader :stack, :print_to_stdout
+
+  alias print_to_stdout? print_to_stdout
+
+  def reset
+    self.statement = nil
+    self.expressions = []
+    @register = 0
+    @stack = []
+    @stdout_log = []
+  end
+
+  def execute(expression)
+    command = find_command(expression)
+    return exec_integer(expression) if command.nil?
+
+    send(command.downcase.to_sym)
+
+    nil
+  end
+
+  def find_command(expression)
+    command = "exec_#{expression.downcase}".to_sym
+    return command if respond_to?(command)
+
+    nil
+  end
+
+  def operate(operation)
+    validate_pop(detail: "Operation: #{operation}")
+    self.register = register.send(operation, stack.pop)
+  end
+
+  def validate_pop(detail: String.new)
+    if stack.size.zero?
+      detail = String.new(" #{detail}") unless detail.empty?
+      raise_error_with_state(StandardError, "The stack was empty.#{detail}")
+    end
+
+    nil
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def raise_error_with_state(error_class, message)
+    message.concat("\n--#{self.class.name} State--")
+    message.concat("\n@expressions=#{expressions.pretty_inspect.strip}")
+    message.concat("\n@register=#{register}")
+    message.concat("\n@stack=#{stack.pretty_inspect.strip}")
+    message.concat("\n@statement=#{statement}")
+    message.concat("\n@stdout_log=#{stdout_log.pretty_inspect}")
+    raise error_class, message
+  end
+  # rubocop:enable Metrics/AbcSize
 end
 
-def minilang(command_string)
-  state = { register: 0, stack: [], command_strings: command_string.split,
-            stdout_log: [] }
+# The OOP program above is based on this procedural version:
+# https://github.com/lightmotive/ls-rb101-exercises/blob/main/11_medium1/06_stack_machine_interpretation.rb
+
+def minilang_test(statement)
+  minilang = Minilang.new(print_to_stdout: false)
 
   begin
-    minilang_iterate!(state)
+    minilang.eval(statement)
+    minilang
   rescue StandardError => e
-    return e
+    e
   end
-
-  state
 end
 
-@print_to_stdout = false
+p minilang_test('PRINT').stdout_log == ['0']
+p minilang_test('5 PUSH 3 MULT PRINT').stdout_log == ['15']
+p minilang_test('5 PRINT PUSH 3 PRINT ADD PRINT').stdout_log == %w[5 3 8]
+p minilang_test('5 PUSH POP PRINT').stdout_log == ['5']
+p minilang_test('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT').stdout_log == %w[5 10 4 7]
+p minilang_test('3 PUSH PUSH 7 DIV MULT PRINT ').stdout_log == ['6']
+p minilang_test('4 PUSH PUSH 7 MOD MULT PRINT ').stdout_log == ['12']
+p minilang_test('-3 PUSH 5 SUB PRINT').stdout_log == ['8']
+# "Exec API" example that replicates statement above
+minilang = Minilang.new(print_to_stdout: false)
+minilang.exec_integer('-3')
+minilang.exec_push
+minilang.exec_integer('5')
+minilang.exec_sub
+minilang.exec_print
+p minilang.stdout_log == ['8']
 
-p minilang('PRINT')[:stdout_log] == ['0']
-p minilang('5 PUSH 3 MULT PRINT')[:stdout_log] == ['15']
-p minilang('5 PRINT PUSH 3 PRINT ADD PRINT')[:stdout_log] == %w[5 3 8]
-p minilang('5 PUSH POP PRINT')[:stdout_log] == ['5']
-p minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')[
-  :stdout_log] ==  %w[5 10 4 7]
-p minilang('3 PUSH PUSH 7 DIV MULT PRINT ')[:stdout_log] == ['6']
-p minilang('4 PUSH PUSH 7 MOD MULT PRINT ')[:stdout_log] == ['12']
-p minilang('-3 PUSH 5 SUB PRINT')[:stdout_log] == ['8']
-p minilang('6 PUSH')[:stdout_log] == []
+p minilang_test('6 PUSH').stdout_log == []
 
 # Further exploration 1:
-# - Try writing a minilang program to evaluate and print the result of this
+# - Try writing a minilang_test program to evaluate and print the result of this
 #   expression: (3 + (4 * 5) - 7) / (5 % 3)
-p minilang('3 PUSH 5 MOD PUSH 7 PUSH 4 PUSH 5 MULT PUSH 3 ' \
-  'ADD SUB DIV PRINT')[:stdout_log] == ['8']
+p minilang_test('3 PUSH 5 MOD PUSH 7 PUSH 4 PUSH 5 MULT PUSH 3 ADD SUB DIV PRINT').stdout_log == ['8']
 # That's easier when one considers math order of operations. You have to work
 # backwards: calculate the values, push them, then calculate the stacked values.
 
@@ -128,26 +165,38 @@ p minilang('3 PUSH 5 MOD PUSH 7 PUSH 4 PUSH 5 MULT PUSH 3 ' \
 # - Add error handling.
 # - Detect when program returns an error, displaying the error message.
 
-result = minilang('6 PUSH POP POP')
+result = minilang_test('6 PUSH POP POP')
 p result.is_a?(StandardError)
 p result.message == <<~MSG
   The stack was empty.
-  --State--
-  {:register=>6, :stack=>[], :command_strings=>[], :stdout_log=>[]}
+  --Minilang State--
+  @expressions=[]
+  @register=6
+  @stack=[]
+  @statement=6 PUSH POP POP
+  @stdout_log=[]
 MSG
 
-result = minilang('6 PUSH ADD ADD')
+result = minilang_test('6 PUSH ADD ADD')
 p result.is_a?(StandardError)
 p result.message == <<~MSG
   The stack was empty. Operation: +
-  --State--
-  {:register=>12, :stack=>[], :command_strings=>[], :stdout_log=>[]}
+  --Minilang State--
+  @expressions=[]
+  @register=12
+  @stack=[]
+  @statement=6 PUSH ADD ADD
+  @stdout_log=[]
 MSG
 
-result = minilang('-3 PUSH 5 SUBTRACT PRINT')
+result = minilang_test('-3 PUSH 5 SUBTRACT PRINT')
 p result.is_a?(ArgumentError)
 p result.message == <<~MSG
-  SUBTRACT is not a valid command.
-  --State--
-  {:register=>5, :stack=>[-3], :command_strings=>["PRINT"], :stdout_log=>[]}
+  SUBTRACT is not a valid expression.
+  --Minilang State--
+  @expressions=["PRINT"]
+  @register=5
+  @stack=[-3]
+  @statement=-3 PUSH 5 SUBTRACT PRINT
+  @stdout_log=[]
 MSG
