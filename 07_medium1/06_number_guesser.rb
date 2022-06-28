2ac5e17ac118d9ef67250b76a54fcb34ec62973d
Andre Light <andrelight.al@gmail.com>
2022-06-28 01:21:29 +0000

    Complete Medium 1 Exercise 6
    - Wondering whether to store `current_guess` or pass it to methods...

--- 07_medium1/06_number_guesser.rb
+++ 07_medium1/06_number_guesser.rb
@@ -1,36 +1,76 @@
 # frozen_string_literal: true
 
+class InvalidGuess < StandardError; end
+
 class GuessingGame
-  def initialize
-    @number = rand(1..100)
-    @guesses_remaining = 7
-  end
+  NUMBER_MIN = 1
+  NUMBER_MAX = 100
 
   def play
-    # loop to get input and check guesses
-    #   - Use methods to check, e.g., `guess_correct = match?`
-    #   - Display results
-    #   - `break if guess_correct`
+    reset
+
+    loop do
+      break display_lost if guesses_remaining.zero?
+
+      prompt_guess
+      break display_won if guess_match?
+
+      display_hint unless guesses_remaining.zero?
+    end
   end
 
   private
 
-  attr_reader :number, :guesses_remaining
+  attr_accessor :number, :guesses_remaining, :current_guess
 
-  def match?(guess)
-    # compare guess with `number`
+  def reset
+    puts "\n"
+    self.number = rand(NUMBER_MIN..NUMBER_MAX)
+    self.guesses_remaining = 7
+    self.current_guess = nil
   end
 
-  def display_status
-    # ...
+  def prompt_guess
+    puts "You have #{guesses_remaining} guesses remaining."
+    loop do
+      print "Enter a number between #{NUMBER_MIN} and #{NUMBER_MAX}: "
+      self.current_guess = input_to_i(gets.chomp)
+      validate_guess
+      self.guesses_remaining -= 1
+      break
+    rescue InvalidGuess
+      print 'Invalid guess. '
+    end
+  end
+
+  def input_to_i(input)
+    # Normally, one would further validate the input as an integer using a
+    # validation library and raise an InvalidInput exception; skipping that
+    # because it isn't explicitly required for this exercise.
+    input.to_i
+  end
+
+  def validate_guess
+    raise InvalidGuess unless current_guess.between?(NUMBER_MIN, NUMBER_MAX)
+  end
+
+  def guess_match?
+    current_guess == number
+  end
+
+  def display_hint
+    message = 'Your guess is too low.' if current_guess < number
+    message = 'Your guess is too high.' if current_guess > number
+
+    puts "#{message}\n\n"
   end
 
   def display_won
-    # ...
+    puts "That's the number!\n\nYou won!"
   end
 
   def display_lost
-    # ...
+    puts "\nYou have no more guesses. You lost!"
   end
 end
 
