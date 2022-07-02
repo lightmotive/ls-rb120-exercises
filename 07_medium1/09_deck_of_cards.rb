7d3c022f8c9234a373e9842884d1bcdd9fe7436e
Andre Light <andrelight.al@gmail.com>
2022-07-02 02:52:40 +0000

    Complete Medium 1 Exercise 9

--- 07_medium1/09_deck_of_cards.rb
+++ 07_medium1/09_deck_of_cards.rb
@@ -1,19 +1,49 @@
 # frozen_string_literal: true
 
+puts "** `Cards` test results: **\n" # For exercise purposes only
 require_relative '08_highest_and_lowest_ranking_cards'
 
 class Deck
   RANKS = ((2..10).to_a + %w[Jack Queen King Ace]).freeze
   SUITS = %w[Hearts Clubs Diamonds Spades].freeze
+
+  def initialize
+    initialize_cards
+  end
+
+  def draw
+    initialize_cards if cards.empty?
+
+    cards.shift
+  end
+
+  private
+
+  attr_accessor :cards
+
+  def initialize_cards
+    self.cards = []
+
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        cards << Card.new(rank, suit)
+      end
+    end
+
+    cards.shuffle!
+
+    nil
+  end
 end
 
 # Examples/tests
+puts '** `Deck` test results: **' # For exercise purposes only
 deck = Deck.new
 drawn = []
 52.times { drawn << deck.draw }
-drawn.count { |card| card.rank == 5 } == 4
-drawn.count { |card| card.suit == 'Hearts' } == 13
+p(drawn.count { |card| card.rank == 5 } == 4)
+p(drawn.count { |card| card.suit == 'Hearts' } == 13)
 
 drawn2 = []
 52.times { drawn2 << deck.draw }
-drawn != drawn2 # Almost always.
+p(drawn != drawn2) # Almost always.
